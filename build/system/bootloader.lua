41915133f5e0b83548c3bc9e52fb76f5f223a755
mirko <mirkogodak@gmail.com>
2024-07-28 15:25:48 +0200

    Add __LEGACY prefix to shell api on bootloader

--- build/system/bootloader.lua
+++ build/system/bootloader.lua
@@ -10,7 +10,7 @@ function main()
     local branch = textutils.unserialiseJSON(http.get("https://api.github.com/repos/mirkokral/ccarcos/commits/main").readAll())["sha"]
     local cur = fs.open("/system/rel", "r")
     if cur and cur.readAll() ~= branch then
-        shell.run("/system/installer.lua")
+        __LEGACY.shell.run("/system/installer.lua")
     end
     print("arcos2 bootloader")
     local args = ""
