54de72bae34b69e09c182020ce164c365fb5f701
mirko <mirkogodak@gmail.com>
2024-07-28 15:47:04 +0200

    asdf

--- src/apps/elevatorStep.lua
+++ src/apps/elevatorStep.lua
@@ -21,22 +21,22 @@ local function contains(table, element)
     return false
 end
 
-tasking.createTask("Elevator step player detector handler", function()
-    local pd = devices.find("playerDetector")
-    while true do
-        local playersInRange = pd.getPlayersInRange(5)
-        for _, i in ipairs(whitelistedPlayers) do
-            print("Testing: " .. i)
-            print(table.unpack(playersInRange))
-            if contains(playersInRange, i) then
-                print("sending")
-                if currentFloor ~= 7 then mdm.transmit(476, 0, 7) end
-                currentFloor = 7
-            end
-        end
-        sleep(1)
-    end
-end, 1, "root", term)
+-- tasking.createTask("Elevator step player detector handler", function()
+--     local pd = devices.find("playerDetector")
+--     while true do
+--         local playersInRange = pd.getPlayersInRange(5)
+--         for _, i in ipairs(whitelistedPlayers) do
+--             print("Testing: " .. i)
+--             print(table.unpack(playersInRange))
+--             if contains(playersInRange, i) then
+--                 print("sending")
+--                 if currentFloor ~= 7 then mdm.transmit(476, 0, 7) end
+--                 currentFloor = 7
+--             end
+--         end
+--         sleep(1)
+--     end
+-- end, 1, "root", term)
 
 while true do
     local _, side, channel, rc, msg, dist = arcos.ev("modem_message")
