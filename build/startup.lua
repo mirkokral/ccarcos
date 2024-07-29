b63598dc90ef3e9f1120b21b1a40862beffd6b86
mirko <mirkogodak@gmail.com>
2024-07-29 14:43:55 +0200

    sussy bakkaaaaaa

--- build/startup.lua
+++ build/startup.lua
@@ -1,3 +1,7 @@
+local oldug = {}
+for k, v in _G do
+    oldug[k] = v
+end
 _G.__LEGACY = {
     colors = colors,
     colours = colours,
@@ -24,27 +28,11 @@ _G.__LEGACY = {
     vector = vector,
     window = window
 }
-_G.colors = nil 
-_G.colours = nil 
-_G.commands = nil 
-_G.disk = nil 
-_G.fs = nil 
-_G.gps = nil 
-_G.help = nil 
-_G.http = nil 
-_G.io = nil 
-_G.keys = nil 
-_G.os = nil 
-_G.paintutils = nil 
-_G.parallel = nil 
-_G.peripheral = nil 
-_G.pocket = nil 
-_G.rednet = nil 
-_G.redstone = nil 
-_G.settings = nil 
-_G.shell = nil 
-_G.textutils = nil 
-_G.turtle = nil 
-_G.vector = nil 
-_G.window = nil 
+setmetatable(_G, {
+    __index = function(_, i) do
+        if debug.getinfo(2).source:sub(2) == "bios.lua" then
+            return oldug[i]
+        end
+    end
+})
 dofile("/system/bootloader.lua")
