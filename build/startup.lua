7bf25998b81d4bd4b4625a7993c3409694b95fcf
mirko <mirkogodak@gmail.com>
2024-07-29 14:56:22 +0200

    fewwwwwweeeee

--- build/startup.lua
+++ build/startup.lua
@@ -1,14 +1,4 @@
 local oldug = {}
-for k, v in pairs(_G) do
-    oldug[k] = v
-end
-setmetatable(_G, {
-    __index = function(_, i)
-        if debug.getinfo(2).source:sub(2) == "bios.lua" then
-            return oldug[i]
-        end
-    end
-})
 _G.__LEGACY = {
     colors = colors,
     colours = colours,
@@ -35,28 +25,31 @@ _G.__LEGACY = {
     vector = vector,
     window = window
 }
-_G["colors"] = nil
-_G["colours"] = nil
-_G["commands"] = nil
-_G["disk"] = nil
-_G["fs"] = nil
-_G["gps"] = nil
-_G["help"] = nil
-_G["http"] = nil
-_G["io"] = nil
-_G["keys"] = nil
-_G["os"] = nil
-_G["paintutils"] = nil
-_G["parallel"] = nil
-_G["peripheral"] = nil
-_G["pocket"] = nil
-_G["rednet"] = nil
-_G["redstone"] = nil
-_G["settings"] = nil
-_G["shell"] = nil
-_G["term"] = nil
-_G["textutils"] = nil
-_G["turtle"] = nil
-_G["vector"] = nil
-_G["window"] = nil
-dofile("/system/bootloader.lua")
+for k, v in pairs(_G) do
+    oldug[k] = v
+end
+oldug["colors"] = nil
+oldug["colours"] = nil
+oldug["commands"] = nil
+oldug["disk"] = nil
+oldug["fs"] = nil
+oldug["gps"] = nil
+oldug["help"] = nil
+oldug["http"] = nil
+oldug["io"] = nil
+oldug["keys"] = nil
+oldug["os"] = nil
+oldug["paintutils"] = nil
+oldug["parallel"] = nil
+oldug["peripheral"] = nil
+oldug["pocket"] = nil
+oldug["rednet"] = nil
+oldug["redstone"] = nil
+oldug["settings"] = nil
+oldug["shell"] = nil
+oldug["term"] = nil
+oldug["textutils"] = nil
+oldug["turtle"] = nil
+oldug["vector"] = nil
+oldug["window"] = nil
+loadfile("/system/bootloader.lua", nil, oldug)
