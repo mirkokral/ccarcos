4583cd47939195e225504d7012c47c2f960df5a5
mirko <mirkogodak@gmail.com>
2024-07-29 15:05:46 +0200

    fix bootloader

--- build/system/bootloader.lua
+++ build/system/bootloader.lua
@@ -1,19 +1,29 @@
 local theme = {
-    background = col.black,
-    foreground = col.white
+    background = __LEGACY.colors.black,
+    foreground = __LEGACY.colors.white
 }
+function mysplit(inputstr, sep)
+    if sep == nil then
+      sep = "%s"
+    end
+    local t = {}
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+      table.insert(t, str)
+    end
+    return t
+  end
 function main()
     term.setTextColor(theme.foreground)
     term.setBackgroundColor(theme.background)
     term.clear()
     term.setCursorPos(1, 1)
-    local branch = textutils.unserialiseJSON(http.get("https://api.github.com/repos/mirkokral/ccarcos/commits/main").readAll())["sha"]
-    local cur = fs.open("/system/rel", "r")
+    local branch = __LEGACY.textutils.unserialiseJSON(__LEGACY.http.get("https://api.github.com/repos/mirkokral/ccarcos/commits/main").readAll())["sha"] 
+    local cur = __LEGACY.fs.open("/system/rel", "r")
     if cur and cur.readAll() ~= branch then
-        __LEGACY.shell.run("/system/installer.lua")
+        laodfile("/system/installer.lua")()
     end
     print("arcos2 bootloader")
     local args = ""
-    _G.__LEGACY.shell.run("/system/krnl.lua " .. args)
+    loadfile("/system/krnl.lua", mysplit(args, " "))
 end
 main()
