52de29eba05946d3c9375cb776f99282f2e57e05
mirko <mirkogodak@gmail.com>
2024-07-30 17:02:40 +0200

    nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn

--- src/startup.lua
+++ src/startup.lua
@@ -10,71 +10,13 @@ else
   print("Update failed")
 end
 
-local oldug = {}
-_G.__LEGACY = {
-    colors = colors,
-    colours = colours,
-    commands = commands,
-    disk = disk,
-    fs = fs,
-    gps = gps,
-    help = help,
-    http = http,
-    io = io,
-    keys = keys,
-    os = os,
-    paintutils = paintutils,
-    parallel = parallel,
-    peripheral = peripheral,
-    pocket = pocket,
-    rednet = rednet,
-    redstone = redstone,
-    settings = settings,
-    shell = shell,
-    term = term,
-    textutils = textutils,
-    turtle = turtle,
-    vector = vector,
-    window = window
-}
-
-for k, v in pairs(_G) do
-    oldug[k] = v
-end
-
-
-oldug["colors"] = nil
-oldug["colours"] = nil
-oldug["commands"] = nil
-oldug["disk"] = nil
-oldug["fs"] = nil
-oldug["gps"] = nil
-oldug["help"] = nil
-oldug["http"] = nil
-oldug["io"] = nil
-oldug["keys"] = nil
-oldug["os"] = nil
-oldug["paintutils"] = nil
-oldug["parallel"] = nil
-oldug["peripheral"] = nil
-oldug["pocket"] = nil
-oldug["rednet"] = nil
-oldug["redstone"] = nil
-oldug["settings"] = nil
-oldug["shell"] = nil
-oldug["term"] = nil
-oldug["textutils"] = nil
-oldug["turtle"] = nil
-oldug["vector"] = nil
-oldug["window"] = nil
 local oldtr = term.redirect
 local oldprr = os.pullEventRaw
 local oldst = os.shutdown
 local olderr = error
 
-_G.term.redirect = function(e)
-    
-end
+_G.term.redirect = function() end
+_G.error = function() end
 function _G.term.native()
     _G.error = olderr
     _G.term.redirect = oldtr
@@ -85,6 +27,64 @@ function _G.term.native()
         term.clear()
         term.setCursorPos(1, 1)
         term.setTextColor(colors.white)
+
+        local oldug = {}
+        _G.__LEGACY = {
+            colors = colors,
+            colours = colours,
+            commands = commands,
+            disk = disk,
+            fs = fs,
+            gps = gps,
+            help = help,
+            http = http,
+            io = io,
+            keys = keys,
+            os = os,
+            paintutils = paintutils,
+            parallel = parallel,
+            peripheral = peripheral,
+            pocket = pocket,
+            rednet = rednet,
+            redstone = redstone,
+            settings = settings,
+            shell = shell,
+            term = term,
+            textutils = textutils,
+            turtle = turtle,
+            vector = vector,
+            window = window
+        }
+
+        for k, v in pairs(_G) do
+            oldug[k] = v
+        end
+
+
+        oldug["colors"] = nil
+        oldug["colours"] = nil
+        oldug["commands"] = nil
+        oldug["disk"] = nil
+        oldug["fs"] = nil
+        oldug["gps"] = nil
+        oldug["help"] = nil
+        oldug["http"] = nil
+        oldug["io"] = nil
+        oldug["keys"] = nil
+        oldug["os"] = nil
+        oldug["paintutils"] = nil
+        oldug["parallel"] = nil
+        oldug["peripheral"] = nil
+        oldug["pocket"] = nil
+        oldug["rednet"] = nil
+        oldug["redstone"] = nil
+        oldug["settings"] = nil
+        oldug["shell"] = nil
+        oldug["term"] = nil
+        oldug["textutils"] = nil
+        oldug["turtle"] = nil
+        oldug["vector"] = nil
+        oldug["window"] = nil
         local ok, err = pcall(loadfile("/system/bootloader.lua", nil, oldug))
         oldug["__LEGACY"].term.write(err)
         sleep(50)
