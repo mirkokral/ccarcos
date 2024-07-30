5900086892628fd2f81b85b10f51a1b2b88ad305
mirko <mirkogodak@gmail.com>
2024-07-30 17:44:55 +0200

    nx

--- src/startup.lua
+++ src/startup.lua
@@ -1,9 +1,11 @@
+print("Terminate to enter shell or wait 1 second to continue boot")
+sleep(1)
 local f = http.get("https://api.github.com/repos/mirkokral/ccarcos/commits/main")
 if f then
   local branch = textutils.unserialiseJSON(f.readAll())["sha"]
   local cur = fs.open("/system/rel", "r")
   if cur and cur.readAll() ~= branch then
-    loadfile("/system/installer.lua")()
+    loadfile("/system/installer.lua", nil, _G)()
   end
   f.close() 
 else
