18f5d5df70e2cc0f2712d5fe2e2fe6a119f2dceb
mirko <mirkogodak@gmail.com>
2024-07-30 17:10:22 +0200

    dyn

--- src/startup.lua
+++ src/startup.lua
@@ -24,74 +24,74 @@ function _G.term.native()
     _G.term.redirect = oldtr
     _G.os.pullEventRaw = oldprr
     _G.os.pullEvent = oldpe
-    function _G.os.shutdown() 
-        print("Successful escape")
-        _G.os.shutdown = oldst
-        term.clear()
-        term.setCursorPos(1, 1)
-        term.setTextColor(colors.white)
+   
 
-        local oldug = {}
-        _G.__LEGACY = {
-            colors = colors,
-            colours = colours,
-            commands = commands,
-            disk = disk,
-            fs = fs,
-            gps = gps,
-            help = help,
-            http = http,
-            io = io,
-            keys = keys,
-            os = os,
-            paintutils = paintutils,
-            parallel = parallel,
-            peripheral = peripheral,
-            pocket = pocket,
-            rednet = rednet,
-            redstone = redstone,
-            settings = settings,
-            shell = shell,
-            term = term,
-            textutils = textutils,
-            turtle = turtle,
-            vector = vector,
-            window = window
-        }
+    print("Successful escape")
+    _G.os.shutdown = oldst
+    term.clear()
+    term.setCursorPos(1, 1)
+    term.setTextColor(colors.white)
 
-        for k, v in pairs(_G) do
-            oldug[k] = v
-        end
+    local oldug = {}
+    _G.__LEGACY = {
+        colors = colors,
+        colours = colours,
+        commands = commands,
+        disk = disk,
+        fs = fs,
+        gps = gps,
+        help = help,
+        http = http,
+        io = io,
+        keys = keys,
+        os = os,
+        paintutils = paintutils,
+        parallel = parallel,
+        peripheral = peripheral,
+        pocket = pocket,
+        rednet = rednet,
+        redstone = redstone,
+        settings = settings,
+        shell = shell,
+        term = term,
+        textutils = textutils,
+        turtle = turtle,
+        vector = vector,
+        window = window
+    }
 
-
-        oldug["colors"] = nil
-        oldug["colours"] = nil
-        oldug["commands"] = nil
-        oldug["disk"] = nil
-        oldug["fs"] = nil
-        oldug["gps"] = nil
-        oldug["help"] = nil
-        oldug["http"] = nil
-        oldug["io"] = nil
-        oldug["keys"] = nil
-        oldug["os"] = nil
-        oldug["paintutils"] = nil
-        oldug["parallel"] = nil
-        oldug["peripheral"] = nil
-        oldug["pocket"] = nil
-        oldug["rednet"] = nil
-        oldug["redstone"] = nil
-        oldug["settings"] = nil
-        oldug["shell"] = nil
-        oldug["term"] = nil
-        oldug["textutils"] = nil
-        oldug["turtle"] = nil
-        oldug["vector"] = nil
-        oldug["window"] = nil
-        local ok, err = pcall(loadfile("/system/bootloader.lua", nil, oldug))
-        oldug["__LEGACY"].term.write(err)
-        sleep(50)
+    for k, v in pairs(_G) do
+        oldug[k] = v
     end
+
+
+    oldug["colors"] = nil
+    oldug["colours"] = nil
+    oldug["commands"] = nil
+    oldug["disk"] = nil
+    oldug["fs"] = nil
+    oldug["gps"] = nil
+    oldug["help"] = nil
+    oldug["http"] = nil
+    oldug["io"] = nil
+    oldug["keys"] = nil
+    oldug["os"] = nil
+    oldug["paintutils"] = nil
+    oldug["parallel"] = nil
+    oldug["peripheral"] = nil
+    oldug["pocket"] = nil
+    oldug["rednet"] = nil
+    oldug["redstone"] = nil
+    oldug["settings"] = nil
+    oldug["shell"] = nil
+    oldug["term"] = nil
+    oldug["textutils"] = nil
+    oldug["turtle"] = nil
+    oldug["vector"] = nil
+    oldug["window"] = nil
+    local ok, err = pcall(loadfile("/system/bootloader.lua", nil, oldug))
+    oldug["__LEGACY"].term.write(err)
+    sleep(50)
 end
 
 coroutine.yield()
