ab6dca20a686a83b13c5a08168585c54004ee87f
mirko <mirkogodak@gmail.com>
2024-08-01 12:46:23 +0200

    s

--- build/system/apis/ui.lua
+++ build/system/apis/ui.lua
@@ -10,40 +10,37 @@ local function blitAtPos(x, y, bgCol, forCol, text)
     term.write(text)
 end
 function Label(b)
-    local config = b
+    local config = {}
+    for i, v in pairs(b) do
+        config[i] = v
+    end
     if not config.col then config.col = UItheme.bg end
     if not config.textCol then config.textCol = UItheme.fg end
-    return {
-        x = config.x,
-        y = config.y,
-        getDrawCommands = function ()
-            local rcbuffer = {}
-            local rx = 0
-            local ry = 0
-            local i = 1
-            while config.label:sub(i, i) do
-                if config.label:sub(i, i) == "\n" then
-                    rx = 0
-                    ry = ry + 1
-                else
-                    table.insert(rcbuffer, {
-                        x = config.x + rx,
-                        y = config.y + ry,
-                        forCol = config.textCol,
-                        bgCol = config.col,
-                        text = config.label:sub(i, i)
-                    })
-                    rx = rx + 1
-                end
-                i = i + 1
+    config.getDrawCommands = function ()
+        local rcbuffer = {}
+        local rx = 0
+        local ry = 0
+        local i = 1
+        while config.label:sub(i, i) do
+            if config.label:sub(i, i) == "\n" then
+                rx = 0
+                ry = ry + 1
+            else
+                table.insert(rcbuffer, {
+                    x = config.x + rx,
+                    y = config.y + ry,
+                    forCol = config.textCol,
+                    bgCol = config.col,
+                    text = config.label:sub(i, i)
+                })
+                rx = rx + 1
             end
-        end,
-        onEvent = function(ev)
-        end,
-        label = config.label,
-        col = config.col,
-        textCol = config.textCol
-    }
+            i = i + 1
+        end
+    end
+    config.onEvent = function(ev)
+    end
+    return config
 end
 function DirectRender(wr)
     local rc
