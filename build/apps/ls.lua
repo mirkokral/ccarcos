d149cc7b88d9f2fac4763ae147ebd2193c8e7184
mirko <mirkogodak@gmail.com>
2024-08-01 08:16:23 +0200

    s

--- build/apps/ls.lua
+++ build/apps/ls.lua
@@ -4,7 +4,11 @@ for _, fp in ipairs(f) do
     if fs.exists(fp) then
         if fs.dir(fp) then
             for _, i in ipairs(fs.ls(fp)) do
-                term.setTextColor(fs.dir(fp) and col.green or col.white)
+                if fs.dir(fp) then
+                    term.setTextColor(col.green)
+                else
+                    term.setTextColor(col.white)
+                end
                 write(i .. " ")
             end
             write("\n")
