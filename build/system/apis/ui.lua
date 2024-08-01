5b549c8ef6932a22fb0ad491ded95fe2cfcb3c53
mirko <mirkogodak@gmail.com>
2024-08-01 16:54:44 +0200

    rfix

--- build/system/apis/ui.lua
+++ build/system/apis/ui.lua
@@ -72,7 +72,7 @@ function ScrollPane(b)
         end
         local rmIndexes = {}
         for index, value in ipairs(dcBuf) do
-            if value.x < 1 or value.x > config.width or value.y < 1 or value.y > config.height then
+            if value.x - config.x < 1 or value.x - config.x > config.width or value.y - config.y < 1 or value.y - config.y > config.height then
                 table.insert(rmIndexes, 1, index)
             end
         end
