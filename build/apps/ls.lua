289b21a5cff6078873bbcf4b030511cea9121846
mirko <mirkogodak@gmail.com>
2024-08-01 08:13:21 +0200

    skill issue

--- build/apps/ls.lua
+++ build/apps/ls.lua
@@ -1,9 +1,9 @@
 local path = ... or environ.workDir
 local f = fs.resolve(path)
-for _, fp in irange(f) do
+for _, fp in ipairs(f) do
     if fs.exists(fp) then
         if fs.dir(fp) then
-            for _, i in irange(fs.ls(fp)) do
+            for _, i in ipairs(fs.ls(fp)) do
                 term.setTextColor(fs.dir(fp) and col.green or col.white)
                 write(i .. " ")
             end
