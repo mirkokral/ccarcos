55a82d44a50bdb3467dc93b27201a136ef18bc02
mirko <mirkogodak@gmail.com>
2024-08-03 07:34:22 +0200

    test

--- build/services/oobe.lua
+++ build/services/oobe.lua
@@ -1,15 +1,27 @@
 local w, h = term.getSize()
 local pages = {}
 local page = 1
-pages[1] = {}
-table.insert(pages[1],
+pages[1] = {
+    ui.Label({
+        label = "An error happened during setup!",
+        x = 2, y = 2,
+        textCol = col.red
+    }),
+    ui.Label({
+        label = "<insert error>",
+        x = 2, y = 4,
+        textCol = col.magenta
+    })
+}
+pages[2] = {}
+table.insert(pages[2],
     ui.Label({
         label = "Welcome to ",
         x = 2,
         y = 2
     })
 )
-table.insert(pages[1],
+table.insert(pages[2],
     ui.Label({
         label = "cc",
         x = 13,
@@ -17,7 +29,7 @@ table.insert(pages[1],
         textCol = col.gray
     })
 )
-table.insert(pages[1],
+table.insert(pages[2],
     ui.Label({
         label = "arcos",
         x = 15,
@@ -25,7 +37,7 @@ table.insert(pages[1],
         textCol = col.cyan
     })
 )
-table.insert(pages[1],
+table.insert(pages[2],
     ui.Label({
         label = ui.Wrap("This wizard will guide you through the basic setup steps of arcos.", w-2),
         x = 2,
@@ -33,10 +45,10 @@ table.insert(pages[1],
         textCol = ui.UItheme.lighterBg
     })
 )
-table.insert(pages[1],
+table.insert(pages[2],
     ui.Button({
         callBack = function ()
-            ui.PageTransition(pages[1], pages[2], false, 1, true, term)
+            ui.PageTransition(pages[2], pages[3], false, 1, true, term)
             page = 2
             return true
         end,
@@ -45,17 +57,32 @@ table.insert(pages[1],
         y = h-1
     })
 )
-pages[2] = {}
-table.insert(pages[2], ui.Label({
+pages[3] = {}
+table.insert(pages[3], ui.Label({
     label = "Select an init",
     x = 2,
     y = 2
 }))
-table.insert(pages[2], ui.Label({
-    label = ui.Wrap("An init is the program that gets started on machine boot.", w-2),
+local is = {}
+for index, value in ipairs(fs.ls("/services/")) do
+    table.insert(is, ui.Button{
+        callBack = function ()
+            local f = 
+            ui.PageTransition(pages[3], pages[4], false, 1, true, term)
+            page = 3
+            return true
+        end,
+        label = value:sub(#value-3)
+    })
+end
+table.insert(pages[3], ui.ScrollPane({
     x = 2,
-    y = 2,
-    textCol = ui.UItheme.lighterBg
+    y = 4,
+    col = ui.UItheme.lighterBg,
+    children = is,
+    height = h - 5,
+    width = w - 2,
+    showScrollBtns = false
 }))
 local ls = true
 while true do
--- src/services/oobe.lua
+++ src/services/oobe.lua
@@ -1,16 +1,29 @@
 local w, h = term.getSize()
 local pages = {}
 local page = 1
+-- Error page
+pages[1] = {
+    ui.Label({
+        label = "An error happened during setup!",
+        x = 2, y = 2,
+        textCol = col.red
+    }),
+    ui.Label({
+        label = "<insert error>",
+        x = 2, y = 4,
+        textCol = col.magenta
+    })
+}
 -- Page 1: Welcome
-pages[1] = {}
-table.insert(pages[1],
+pages[2] = {}
+table.insert(pages[2],
     ui.Label({
         label = "Welcome to ",
         x = 2,
         y = 2
     })
 )
-table.insert(pages[1],
+table.insert(pages[2],
     ui.Label({
         label = "cc",
         x = 13,
@@ -18,7 +31,7 @@ table.insert(pages[1],
         textCol = col.gray
     })
 )
-table.insert(pages[1],
+table.insert(pages[2],
     ui.Label({
         label = "arcos",
         x = 15,
@@ -26,7 +39,7 @@ table.insert(pages[1],
         textCol = col.cyan
     })
 )
-table.insert(pages[1],
+table.insert(pages[2],
     ui.Label({
         label = ui.Wrap("This wizard will guide you through the basic setup steps of arcos.", w-2),
         x = 2,
@@ -34,10 +47,10 @@ table.insert(pages[1],
         textCol = ui.UItheme.lighterBg
     })
 )
-table.insert(pages[1],
+table.insert(pages[2],
     ui.Button({
         callBack = function ()
-            ui.PageTransition(pages[1], pages[2], false, 1, true, term)
+            ui.PageTransition(pages[2], pages[3], false, 1, true, term)
             page = 2
             return true
         end,
@@ -47,19 +60,33 @@ table.insert(pages[1],
     })
 )
 -- Page 2
-pages[2] = {}
+pages[3] = {}
 
-table.insert(pages[2], ui.Label({
+table.insert(pages[3], ui.Label({
     label = "Select an init",
     x = 2,
     y = 2
 }))
-
-table.insert(pages[2], ui.Label({
-    label = ui.Wrap("An init is the program that gets started on machine boot.", w-2),
+local is = {}
+for index, value in ipairs(fs.ls("/services/")) do
+    table.insert(is, ui.Button{
+        callBack = function ()
+            local f = 
+            ui.PageTransition(pages[3], pages[4], false, 1, true, term)
+            page = 3
+            return true
+        end,
+        label = value:sub(#value-3)
+    })
+end
+table.insert(pages[3], ui.ScrollPane({
     x = 2,
-    y = 2,
-    textCol = ui.UItheme.lighterBg
+    y = 4,
+    col = ui.UItheme.lighterBg,
+    children = is,
+    height = h - 5,
+    width = w - 2,
+    showScrollBtns = false
 }))
 -- Rendering
 local ls = true
