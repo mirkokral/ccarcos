1e5c588a90dc466dd93c449a513523457fd27604
mirko <mirkogodak@gmail.com>
2024-08-03 21:07:37 +0200

    asus sus

--- repo/test/apps/test.lua
+++ repo/test/apps/test.lua
@@ -3,6 +3,11 @@ local page = {
         label = "Hello, world!",
         x = 2,
         y = 2
+    },
+    ui.Label{
+        label = ui.Wrap("This is a test of the UI wrapping, this should wrap around as it's quite long.", ({ term.getSize() })[2]-2),
+        x = 2,
+        y = 4
     }
 }
 
