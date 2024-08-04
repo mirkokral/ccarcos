258771b3ea7520b7347ad4dee7218655d0ace005
mirko <mirkogodak@gmail.com>
2024-08-04 12:19:16 +0200

    sq

--- build/system/apis/ui.lua
+++ build/system/apis/ui.lua
@@ -433,15 +433,15 @@ function RenderWidgets(wdg, ox, oy, buf)
     end
 end
 function PageTransition(widgets1, widgets2, dir, speed, ontop, terma)
+    local tw, th = terma.getSize()
+    local ox = 0
+    local buf = InitBuffer(terma)
+    local buf2 = InitBuffer(terma)
+    local accel = 50
+    RenderWidgets(widgets1, 0, 0, buf)
+    RenderWidgets(widgets2, 0, 0, buf2)
+    speed = speed + 1
     if ontop then
-        local tw, th = terma.getSize()
-        local ox = 0
-        local buf = InitBuffer(terma)
-        local buf2 = InitBuffer(terma)
-        local accel = 50
-        RenderWidgets(widgets1, 0, 0, buf)
-        RenderWidgets(widgets2, 0, 0, buf2)
-        speed = speed + 1
         while ox < tw-0.5 do
             ox = math.max(((ox/tw)+(accel/100))*tw, 0)
             accel = accel/speed
@@ -452,14 +452,13 @@ function PageTransition(widgets1, widgets2, dir, speed, ontop, terma)
             sleep(1/20)
         end
     else
-        while ox < tw do
-            ox = math.min(ox + accel, tw)
-            accel = accel * speed
+        while ox < tw-0.5 do
+            ox = math.max(((ox/tw)+(accel/100))*tw, 0)
+            accel = accel/speed
             local sbuf = InitBuffer(terma)
             Cpy(buf2, sbuf, 0, 0)
-            Cpy(buf, sbuf, ox * (dir and -1 or 1), 0)
+            Cpy(buf, sbuf, (ox) * (dir and -1 or 1), 0)
             Push(sbuf, terma)
-            print(accel)
             sleep(1/20)
         end
     end
