8f4ff0bdec8281327be9c53c74643fd2d72c3c6d
emir4169 <em.ireri41@gmail.com>
2024-08-09 19:06:33 +0300

    Update compat.lua

--- repo/bmoscompat/apps/compat.lua
+++ repo/bmoscompat/apps/compat.lua
@@ -2,9 +2,55 @@ local bmos_compat_env = {}
 for key, value in pairs(_G) do
     bmos_compat_env[key] = value
 end
+local directory = "/"
+resolvePath = function(path)
+	local matches = {}
+	for i in path:gmatch("[^/]+") do
+		table.insert(matches,i)
+	end
+	local result1 = {}
+	local lastIndex = 1
+	for i,v in pairs(matches) do
+		if v ~= "." then
+			if v== ".." then
+				result1[lastIndex] = nil
+				lastIndex = lastIndex-1
+			else
+				lastIndex = lastIndex + 1
+				result1[lastIndex] = v
+			end
+		end
+	end
+	local result = {}
+	for i,v in pairs(result1) do
+		table.insert(result,v)
+	end
+	local final = "/"
+	for i,v in pairs(result) do
+		if i ~= 1 then
+			final = final .. "/"
+		end
+		final = final..v
+	end
+	return final
+end
 bmos_compat_env._G = setmetatable({}, {__index = bmos_compat_env, __newindex = bmos_compat_env})
 bmos_compat_env.rootColor = col.red
 bmos_compat_env.userColor = col.green
+bmos_compat_env.fs.resolvePath = resolvePath
+bmos_compat_env.fs.setDir = function(dir)
+	directory = dir
+end
+bmos_compat_env.fs.getDir = function()
+	return directory
+end
+ bmos_compat_env.fs.getBootedDrive = function()
+	local drive = resolvePath(parentDir.."..").."/"
+	if drive == "//" then
+		drive = "/"
+	end
+	return drive
+end
 bmos_compat_env.user = {
 	login = tasking.changeUser,
 	createUser = arcos.createUser,
