f00fd5b8fcc5cc6d6a02c03f5c2e588bc54aa437
mirko <mirkogodak@gmail.com>
2024-08-10 14:07:20 +0200

    adduser rmuser in objList

---
+++ build/apps/rmuser.lua
@@ -0,0 +1,16 @@
+if arcos.getCurrentTask().user ~= "root" then
+    write("[escalation] Enter root password: ")
+    local pass = read("*")
+    local f = tasking.changeUser("root", pass)
+    if not f then
+        error("Invalid password!")
+    end
+end
+local args = { ... }
+if #args ~= 1 then
+    error("Too many or too few args.")
+end
+local u = arcos.deleteUser(args[1])
+if not u then
+    print("Failed removing user.")
+end
