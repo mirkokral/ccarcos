aa6f1985b770232e0f57076402db94927930bc34
Mirkokral <mirkogodak@gmail.com>
2024-09-20 11:03:06 +0200

    Run /rom/startup.lua on shell

--- repo/craft/data/craft/util/startup.lua
+++ repo/craft/data/craft/util/startup.lua
@@ -1,6 +1,8 @@
 shell.setDir(environ.workDir)
+shell.setAlias(     )
 term.setBackgroundColor(col.black)
 term.setTextColor(col.white)
 term.clear()
 term.setCursorPos(1, 1)
+shell.run("/rom/startup.lua")
 shell.run("shell", ...)
