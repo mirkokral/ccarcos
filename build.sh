a469a7143615e6c2b52aa96fbf9e9cc147e4874e
Mirkokral <mirkogodak@gmail.com>
2024-09-24 11:33:42 +0200

    Package cellui into the base package

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
+# Build arcos
 python build.py clean
 python build.py whole
+
+# Add cellui to build
+cd cellui
+haxe cellui.hxml
+cp out/celluilib.lua ../build/system/apis/cellui.lua
+cd ..
+
+# Put build into the package
 python archivebuild.py
